5dca76875f6943a125672fe1bc7bdb90e671c923
aspenne <axel.mch@outlook.fr>
2024-08-24 18:02:43 +0200

    fix(CD): test, update robot sitemap extension and private key content

--- src/app/robot.ts
+++ src/app/robot.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: "/private/",
     },
-    sitemap: "https://houest-atelier.fr/sitemap.ts",
+    sitemap: "https://houest-atelier.fr/sitemap.xml",
   };
 }
